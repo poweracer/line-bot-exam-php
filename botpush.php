7139c64b4662f85d72a0803dca824ba27353f126
poweracer <39103576+poweracer@users.noreply.github.com>
2018-05-09 04:22:36 +0700

    Update botpush.php

--- botpush.php
+++ botpush.php
@@ -4,9 +4,9 @@
 
 require "vendor/autoload.php";
 
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = 'm3edsF8K/XFewV6Td722LIJdz2TRlyThaocqJcZxg2LbTU4wZOYqhi/NLmXlLhOPVo2AbLYmn4OOLWowNZx3/JPksAwzDwegEUGzu4xAiAQjHtBdoUwBDsuYSZISbJndqkmqE/Q6AzJPvmzCt/ejkQdB04t89/1O/w1cDnyilFU=';
 
-$channelSecret = '75c03f392f6e53d662d6f5a8db9e421f';
+$channelSecret = '8668b7b5a0a6a607719e5b8ac888fbee';
 
 $pushID = 'U7ef7a449f2a5c2057eacfc02ba2eb286';
 
